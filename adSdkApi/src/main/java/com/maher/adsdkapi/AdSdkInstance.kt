1e0b1e868bea14f54b3683d3f0b44554191379f7
maher gafsi <mgafsi@payplug.com>
2024-07-11 14:02:57 +0200

    add sdk api library

--- adSdkApi/src/main/java/com/maher/adsdkapi/AdSdkInstance.kt
+++ adSdkApi/src/main/java/com/maher/adsdkapi/AdSdkInstance.kt
@@ -1,6 +1,25 @@
 package com.maher.adsdkapi
 
+import android.content.Context
+import com.maher.ad_sdk.AdSdk
+import com.maher.ad_sdk.AdSdkImpl
+import com.maher.ad_sdk.common.AdSdkException
+import com.maher.ad_sdk.domain.AdEventType
+import java.util.UUID
+
 object AdSdkInstance {
     private val adSdk: AdSdk = AdSdkImpl()
 
+    fun initialize(key: UUID) {
+        adSdk.initialize(key)
+    }
+
+    @Throws(AdSdkException::class)
+    fun show(context: Context) {
+        adSdk.show(context)
+    }
+
+    fun trackEvent(event: AdEventType) {
+        adSdk.trackEvent(event)
+    }
 }
